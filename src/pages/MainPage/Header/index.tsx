b8312f89e5996b1555658514a3eb16815e2ca639
bruceeewong <292803281@qq.com>
2022-08-14 01:30:09 -0400

    feat: renew header styles

--- src/pages/MainPage/Header/index.tsx
+++ src/pages/MainPage/Header/index.tsx
@@ -1,14 +1,28 @@
-import "./index.scss";
+import styles from "./index.module.scss";
+import AvatarDefault from '../../../assets/avatars/default.svg';
+import IconArrowDown from '../../../assets/icons/arrow-down.svg';
+import classnames from "classnames";
+
+const Avatar = () => {
+  return (
+    <div className={styles['avatar']}>
+      <img src={AvatarDefault} alt="avatar" />
+    </div>  
+  )
+}
 
 function Header() {
   return (
-    <div className="header-container">
-      <div className="avatar" />
-      <div className="num">Account1</div>
-      <div className="account">
+    <div className={styles['header-container']}>
+      <Avatar />
+      <div className={styles['account']}>
+        <span>Account1</span>  
+        <img className="ml-[6px]" src={IconArrowDown} alt="arrow down" />
+      </div>
+      <div className={classnames(styles['address'], 'ml-[18px]')}>
         0x2152f....01f6 <span></span>
       </div>
-      <div className="net">devnet</div>
+      <div className={styles['net']}>devnet</div>
     </div>
   );
 }
