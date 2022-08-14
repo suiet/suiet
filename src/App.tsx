1a630c74112bc7513bd19025cfd006acab7a596f
bruceeewong <292803281@qq.com>
2022-08-13 23:40:52 -0400

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import LoginPage from "./components/LoginPage";
 import MainPage from "./components/MainPage";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <MainPage />
-    </>
+    <Routes>
+      <Route path='/' element={<MainPage />} />
+      <Route path='login' element={<LoginPage />} />
+    </Routes>
   );
 }
 
