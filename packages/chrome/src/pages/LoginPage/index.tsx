6fed93b598bfd18267b15ce3f11b99a811846f23
bruceeewong <292803281@qq.com>
2022-08-21 11:52:17 -0400

    feat: restyle welcome page

--- packages/chrome/src/pages/LoginPage/index.tsx
+++ packages/chrome/src/pages/LoginPage/index.tsx
@@ -1,12 +1,48 @@
 import styles from "./index.module.scss";
 import {Link} from "react-router-dom";
+import {Extendable} from "../../types";
+import classnames from "classnames";
+import Typo from "../../components/Typo";
 
-export default function MainPage() {
+type LinkButtonProps = Extendable & {
+  to: string;
+  type?: 'primary' | 'default';
+}
+
+const LinkButton = (props: LinkButtonProps) => {
+  const {type = 'default'} = props;
+  return (
+    <Link to={props.to}>
+      <div className={classnames(
+        styles['link-btn'],
+        styles[`link-btn--${type}`],
+        props.className)}
+      >{props.children}</div>
+    </Link>
+  )
+}
+
+const Welcome = () => {
   return (
     <div className={styles['main-page']}>
-      <div className={styles['suiet-title']}>Suiet</div>
-      <div className={styles['create-new-btn']}><Link to={'/settings'}>Create New</Link></div>
-      <div className={styles['import-wallet-btn']}>Import Wallet</div>
+      <Typo.Title className={styles['suiet-title']}>Suiet</Typo.Title>
+      <Typo.Normal className={styles['suiet-desc']}>The wallet for Everyone</Typo.Normal>
+      <div className={'mt-[117px]'}>
+        <LinkButton
+          to={'/settings'}
+          type={'primary'}
+        >Create New</LinkButton>
+        <LinkButton
+          to={'/settings'}
+          className={'mt-[16px]'
+          }>Import Wallet</LinkButton>
+      </div>
     </div>
+  )
+}
+
+export default function LoginPage() {
+  return (
+    <Welcome />
   );
 }
