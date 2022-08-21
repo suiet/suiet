7a19292e66700f95463b02008f752fb0fb24a587
bruceeewong <292803281@qq.com>
2022-08-21 15:38:58 -0400

    feat: add Set password page

--- packages/chrome/src/pages/LoginPage/index.tsx
+++ packages/chrome/src/pages/LoginPage/index.tsx
@@ -1,48 +1,9 @@
-import styles from "./index.module.scss";
-import {Link} from "react-router-dom";
-import {Extendable} from "../../types";
-import classnames from "classnames";
-import Typo from "../../components/Typo";
-
-type LinkButtonProps = Extendable & {
-  to: string;
-  type?: 'primary' | 'default';
-}
-
-const LinkButton = (props: LinkButtonProps) => {
-  const {type = 'default'} = props;
-  return (
-    <Link to={props.to}>
-      <div className={classnames(
-        styles['link-btn'],
-        styles[`link-btn--${type}`],
-        props.className)}
-      >{props.children}</div>
-    </Link>
-  )
-}
-
-const Welcome = () => {
-  return (
-    <div className={styles['main-page']}>
-      <Typo.Title className={styles['suiet-title']}>Suiet</Typo.Title>
-      <Typo.Normal className={styles['suiet-desc']}>The wallet for Everyone</Typo.Normal>
-      <div className={'mt-[117px]'}>
-        <LinkButton
-          to={'/settings'}
-          type={'primary'}
-        >Create New</LinkButton>
-        <LinkButton
-          to={'/settings'}
-          className={'mt-[16px]'
-          }>Import Wallet</LinkButton>
-      </div>
-    </div>
-  )
-}
+import Welcome from "./Welcome";
+import SetPassword from "./SetPassword";
 
 export default function LoginPage() {
   return (
-    <Welcome />
+    // <Welcome />
+    <SetPassword />
   );
 }
