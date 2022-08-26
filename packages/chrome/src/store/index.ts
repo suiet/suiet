12a71e6990f6b6fcc551d950b5a275d5578ef196
bruceeewong <292803281@qq.com>
2022-08-26 18:42:33 -0400

    feat(ext): add redux-persist ability

--- packages/chrome/src/store/index.ts
+++ packages/chrome/src/store/index.ts
@@ -1,11 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import appContextReducer from './app-context'
+import {ChromeStorage} from "./persist-storage";
+import {persistReducer, persistStore} from 'redux-persist'
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
 
-export const store = configureStore({
-  reducer: {
-    appContext: appContextReducer
-  }
+const persistConfig = {
+  key: 'root',
+  storage: new ChromeStorage(),
+  whitelist: [
+    'appContext'
+  ]
+}
+
+const allReducers = combineReducers({
+  appContext: appContextReducer
 })
+
+function createStore() {
+  const store = configureStore({
+    reducer: persistReducer(
+      persistConfig,
+      allReducers,
+    ),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+      })
+  })
+  const persistorStore = persistStore(store);
+  return { store, persistorStore };
+}
+
+export const {store, persistorStore} = createStore()
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
