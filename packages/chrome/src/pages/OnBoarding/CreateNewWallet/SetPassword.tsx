2fccd3968bb2f0ec98b70f83e65b1980b092797f
bruceeewong <292803281@qq.com>
2022-08-25 23:31:05 -0400

    chore(ext): extract password input validation

--- packages/chrome/src/pages/OnBoarding/CreateNewWallet/SetPassword.tsx
+++ packages/chrome/src/pages/OnBoarding/CreateNewWallet/SetPassword.tsx
@@ -4,12 +4,26 @@ import classnames from "classnames";
 import LinkButton from "../LinkButton";
 import Input from "../../../components/Input";
 import {CreateWalletStepProps} from "./index";
+import {useForm} from "react-hook-form";
+import {ErrorMessage} from "@hookform/error-message";
+import React from "react";
+import {getInputStateByFormState, getPasswordValidation} from "../../../utils/form";
 
 export type SavePasswordProps = {
   onNext: (password: string) => void;
 }
 
+type FormData = {
+  password: string;
+}
+
 const SavePassword = (props: SavePasswordProps) => {
+  const {register, handleSubmit, formState} = useForm<FormData>()
+
+  async function unlock(data: FormData) {
+    props.onNext('123456')
+  }
+
   return (
     <div className={styles['container']}>
       <Typo.Title className={
@@ -19,21 +33,34 @@ const SavePassword = (props: SavePasswordProps) => {
         )
       }>Set wallet password</Typo.Title>
       <section className={'mt-[214px] w-full px-[22px]'}>
+        <form onSubmit={handleSubmit(unlock)}>
         <div>
           <Typo.Small className={styles['pwd']}>Password</Typo.Small>
-          <Input className={'mt-[6px]'} placeholder={'Please enter the password'} />
+          <Input
+            {...register('password', getPasswordValidation())}
+            state={getInputStateByFormState(formState, 'password')}
+            className={'mt-[6px]'}
+            placeholder={'Please enter the password'}
+          />
+          <ErrorMessage
+            errors={formState.errors}
+            name={'password'}
+            render={(error) => (
+              <Typo.Hints state={'error'} className={'mt-[6px]'}>{error.message}</Typo.Hints>
+            )}
+          />
         </div>
         <LinkButton
-          to={'/settings'}
+          type={'submit'}
           theme={'primary'}
           className={classnames(
             styles['step-button'],
             'mt-[16px]'
           )}
-          onClick={() => {props.onNext('123456')}}
         >
           Next
         </LinkButton>
+        </form>
       </section>
     </div>
   )
