f13cdce2e63f1518f0549358be288d8bf28b6379
bruceeewong <292803281@qq.com>
2022-08-25 23:45:47 -0400

    feat(ext): add copy-to-clipboard function

--- packages/chrome/src/pages/MainPage/Dashboard/index.tsx
+++ packages/chrome/src/pages/MainPage/Dashboard/index.tsx
@@ -13,6 +13,7 @@ import classnames from "classnames";
 import {useState} from "react";
 import toast from "../../../components/toast";
 import {ReactComponent as IconSuccess} from "../../../assets/icons/toast-success.svg";
+import copy from "copy-to-clipboard";
 
 export type ReceiveButtonProps = {
   address: string;
@@ -36,11 +37,6 @@ const ReceiveButton = (props: ReceiveButtonProps) => {
     >
       <div className={'flex flex-col items-center mt-[22px]'}>
         <div className={'flex flex-col items-center'}>
-          {/*<img*/}
-          {/*  src={TestQrCode}*/}
-          {/*  alt="qr-code"*/}
-          {/*  className={styles['qr-code']}*/}
-          {/*/>*/}
           <QRCodeSVG value={props.address} className={styles['qr-code']} />
           <Typo.Normal className={classnames('mt-[2px]', styles['text-scan'])}>
             scan to receive
@@ -48,7 +44,10 @@ const ReceiveButton = (props: ReceiveButtonProps) => {
         </div>
         <div className={'flex items-center mt-[21px]'}>
           <Typo.Small>0x2152f01152f01f6152f01f6f6</Typo.Small>
-          <CopyIcon className={'ml-[5px]'}></CopyIcon>
+          <CopyIcon className={'ml-[5px]'} onClick={() => {
+            copy(props.address);
+            toast.success('Copied Address')
+          }}></CopyIcon>
         </div>
       </div>
     </Modal>
