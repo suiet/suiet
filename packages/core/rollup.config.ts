d7ef0f5395aeb3c56fab0557410cda7749ab8d0b
bruceeewong <292803281@qq.com>
2022-08-27 17:52:55 -0400

    ci(core): add rollup to bundle codes

---
+++ packages/core/rollup.config.ts
@@ -0,0 +1,28 @@
+import resolvePlugin from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import cjs2es from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+import {defineConfig} from "rollup";
+import { babel } from '@rollup/plugin-babel';
+import * as path from "path";
+
+export default defineConfig({
+  input: 'src/index.ts',
+  output: {
+    format: 'es',
+    dir: 'dist',
+  },
+  plugins: [
+    nodePolyfills(),
+    resolvePlugin(),
+    typescript({
+      tsconfig: path.resolve(__dirname, 'tsconfig.json')
+    }),
+    cjs2es(), // must place before babel
+    babel({
+      babelHelpers: 'bundled'
+    }),
+    json(),
+  ]
+})
