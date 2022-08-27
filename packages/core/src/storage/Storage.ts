4649220aa59b75dfa00c70d26039f496226bf7a7
5kbpers <hustmh@gmail.com>
2022-08-28 02:27:05 +0800

    feat(storage): update storage interface
    Signed-off-by: 5kbpers <hustmh@gmail.com>

--- packages/core/src/storage/Storage.ts
+++ packages/core/src/storage/Storage.ts
@@ -1,20 +1,20 @@
 import { Account, Wallet, GlobalMeta } from "./types";
 
-export abstract class Storage {
-    abstract getWallets(): Promise<Array<Wallet>>;
-    abstract getWallet(id: string): Promise<Wallet | null>;
+export interface Storage {
+    getWallets(): Promise<Array<Wallet>>;
+    getWallet(id: string): Promise<Wallet | null>;
 
-    abstract addWallet(id: string, wallet: Wallet): Promise<void>;
-    abstract updateWallet(id: string, wallet: Wallet): Promise<void>;
-    abstract deleteWallet(id: string): Promise<void>;
+    addWallet(id: string, wallet: Wallet): Promise<void>;
+    updateWallet(id: string, wallet: Wallet): Promise<void>;
+    deleteWallet(id: string): Promise<void>;
 
-    abstract getAccounts(walletId: string): Promise<Array<Account>>;
-    abstract getAccount(walletId: string, accountId: string): Promise<Account | null>;
+    getAccounts(walletId: string): Promise<Array<Account>>;
+    getAccount(walletId: string, accountId: string): Promise<Account | null>;
 
-    abstract addAccount(walletId: string, accountId: string, account: Account): Promise<void>;
-    abstract updateAccount(walletId: string, accountId: string, account: Account): Promise<void>;
-    abstract deleteAccount(walletId: string, accountId: string): Promise<void>;
+    addAccount(walletId: string, accountId: string, account: Account): Promise<void>;
+    updateAccount(walletId: string, accountId: string, account: Account): Promise<void>;
+    deleteAccount(walletId: string, accountId: string): Promise<void>;
 
-    abstract loadMeta(): Promise<GlobalMeta | null>;
-    abstract saveMeta(meta: GlobalMeta): Promise<void>;
+    loadMeta(): Promise<GlobalMeta | null>;
+    saveMeta(meta: GlobalMeta): Promise<void>;
 }
