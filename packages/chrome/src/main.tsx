9ed0155f65b2ac07fc1d8d68db276555ad44303b
bruceeewong <292803281@qq.com>
2022-09-03 15:53:12 -0400

    feat(ext): balance loading & cache

--- packages/chrome/src/main.tsx
+++ packages/chrome/src/main.tsx
@@ -6,13 +6,17 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { persistorStore, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { SWRConfig } from 'swr';
+import { swrConfig } from './configs/swr';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MemoryRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistorStore}>
-          <App />
+          <SWRConfig value={swrConfig}>
+            <App />
+          </SWRConfig>
         </PersistGate>
       </Provider>
     </MemoryRouter>
