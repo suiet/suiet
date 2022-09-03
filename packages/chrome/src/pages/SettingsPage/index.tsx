faa2733bcf8127c3262b1058405e097c325bbc48
bruceeewong <292803281@qq.com>
2022-09-02 23:37:57 -0400

    refactor: refresh setting main page

--- packages/chrome/src/pages/SettingsPage/index.tsx
+++ packages/chrome/src/pages/SettingsPage/index.tsx
@@ -7,15 +7,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { resetAppContext } from '../../store/app-context';
 import { AppDispatch, RootState } from '../../store';
 import { useAccount } from '../../hooks/useAccount';
-import CopyIcon from '../../components/CopyIcon';
-import toast from '../../components/toast';
-import { addressEllipsis } from '../../utils/format';
 import { coreApi } from '@suiet/core';
-import { avatarMap } from '../../constants/avatar';
 import { isDev } from '../../utils/env';
 import Address from '../../components/Address';
+import Avatar from '../../components/Avatar';
+import classnames from 'classnames';
+import manifest from '../../utils/manifest';
 
-function SettingPage() {
+const SettingMain = () => {
   const navigate = useNavigate();
   const token = useSelector((state: RootState) => state.appContext.token);
   const dispatch = useDispatch<AppDispatch>();
@@ -31,72 +30,79 @@ function SettingPage() {
   }
 
   return (
-    <div className={styles['settings-container']}>
+    <div className={styles['container']}>
+      <div className={'flex flex-col items-center'}>
+        <Avatar size={'large'}></Avatar>
+        <div className={classnames(styles['wallet-name'], 'mt-[8px]')}>
+          {wallet.name}
+        </div>
+        <Address value={account.address} className={styles['address']} />
+      </div>
+
+      <section className={styles['settings-container']}>
+        <div
+          onClick={() => {
+            navigate('wallet', {
+              state: {
+                hideApplayout: true,
+              },
+            });
+          }}
+          className={styles['settings-item']}
+        >
+          <span className={styles['icon-wallet']}></span>Wallet
+          <span className={styles['icon-right-arrow']} />
+        </div>
+        <div
+          onClick={() => {
+            navigate('network', {
+              state: {
+                hideApplayout: true,
+              },
+            });
+          }}
+          className={styles['settings-item']}
+        >
+          <span className={styles['icon-network']}></span>Network
+          <span className={styles['icon-right-arrow']} />
+        </div>
+        <div
+          onClick={() => {
+            navigate('security', {
+              state: {
+                hideApplayout: true,
+              },
+            });
+          }}
+          className={styles['settings-item']}
+        >
+          <span className={styles['icon-security']}></span>Security
+          <span className={styles['icon-right-arrow']} />
+        </div>
+        {/* dev use */}
+        {isDev && (
+          <div
+            onClick={async () => await handleResetApp()}
+            className={styles['settings-item']}
+          >
+            <span className={styles['icon-security']}></span>
+            Reset App
+          </div>
+        )}
+      </section>
+
+      <div className={classnames(styles['app-version'], 'mt-[16px]')}>
+        version v{manifest.version}
+      </div>
+    </div>
+  );
+};
+
+function SettingPage() {
+  return (
+    <div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div
-                className={styles['wallet-avatar']}
-                style={{
-                  backgroundImage: `url('${avatarMap[wallet.avatar]}')`,
-                }}
-              ></div>
-              <div className={styles['wallet-name']}>{wallet.name}</div>
-              <Address value={account.address} className={styles['address']} />
-              <div
-                onClick={() => {
-                  navigate('wallet', {
-                    state: {
-                      hideApplayout: true,
-                    },
-                  });
-                }}
-                className={styles['settings-item']}
-              >
-                <span className={styles['icon-wallet']}></span>Wallet
-                <span className={styles['icon-right-arrow']} />
-              </div>
-              <div
-                onClick={() => {
-                  navigate('network', {
-                    state: {
-                      hideApplayout: true,
-                    },
-                  });
-                }}
-                className={styles['settings-item']}
-              >
-                <span className={styles['icon-network']}></span>Network
-                <span className={styles['icon-right-arrow']} />
-              </div>
-              <div
-                onClick={() => {
-                  navigate('security', {
-                    state: {
-                      hideApplayout: true,
-                    },
-                  });
-                }}
-                className={styles['settings-item']}
-              >
-                <span className={styles['icon-security']}></span>Security
-                <span className={styles['icon-right-arrow']} />
-              </div>
-              {/* dev use */}
-              {isDev && (
-                <div
-                  onClick={async () => await handleResetApp()}
-                  className={styles['settings-item']}
-                >
-                  <span className={styles['icon-security']}></span>Reset App
-                </div>
-              )}
-              <div className={styles['app-version']}>version v0.0.1</div>
-            </>
-          }
-        />
+        <Route path="/" element={<SettingMain />} />
         <Route path="wallet" element={<Wallet />} />
         <Route path="network" element={<Network />} />
         <Route path="security" element={<Security />} />
