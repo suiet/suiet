9358542f6a913020513bf089815e35fc291eb9e2
brrruski <sizhe4real@gmail.com>
2022-09-04 11:54:26 -0400

    Dev bruski (#59)
    feat: token list

--- packages/chrome/src/pages/MainPage/TokenList/index.tsx
+++ packages/chrome/src/pages/MainPage/TokenList/index.tsx
@@ -1,34 +1,58 @@
 import classnames from 'classnames';
-import type { StyleExtendable } from '../../../types';
+import type { Extendable, StyleExtendable } from '../../../types';
 import IconWaterDrop from '../../../assets/icons/waterdrop.svg';
 import styles from './index.module.scss';
 import TokenIcon from '../../../components/TokenIcon';
 import { useSupportedCoins } from '../../../hooks/useSupportedCoins';
+import Typo from '../../../components/Typo';
+import { useCoinBalance } from '../../../hooks/useCoinBalance';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { useAccount } from '../../../hooks/useAccount';
 
 export type TokenListProps = StyleExtendable;
 
-const TokenItem = () => {
+type TokenItemProps = Extendable & {
+  symbol: string;
+  amount?: number | string;
+};
+
+const TokenItem = (props: TokenItemProps) => {
+  const { amount = 0 } = props;
   return (
     <div className={styles['token-item']}>
       <div className="flex items-center">
         <TokenIcon icon={IconWaterDrop} alt="water-drop" />
-        <strong className={classnames(styles['token-name'], 'ml-[12px]')}>
-          SUI
-        </strong>
-        <p className={classnames(styles['token-amonut'], 'ml-[120px]')}>1000</p>
+        <div className={'flex flex-col ml-[32px]'}>
+          <Typo.Normal className={classnames(styles['token-name'])}>
+            {props.symbol}
+          </Typo.Normal>
+          <Typo.Small className={classnames(styles['token-amount'])}>
+            {amount ?? '0'}
+          </Typo.Small>
+        </div>
       </div>
     </div>
   );
 };
 
 const TokenList = (props: TokenListProps) => {
+  const appContext = useSelector((state: RootState) => state.appContext);
   const { data: supportedCoins } = useSupportedCoins();
-  console.log('supportedCoins', supportedCoins);
+  const { account } = useAccount(appContext.accountId);
+  const { getBalance } = useCoinBalance(account.address);
+
   if (!supportedCoins) return null;
   return (
     <div className={classnames(props.className)} style={props.style}>
       {supportedCoins.map((coin) => {
-        return <TokenItem key={coin.packageId} />;
+        return (
+          <TokenItem
+            key={coin.packageId}
+            symbol={coin.symbol}
+            amount={getBalance(coin.symbol)}
+          />
+        );
       })}
     </div>
   );
