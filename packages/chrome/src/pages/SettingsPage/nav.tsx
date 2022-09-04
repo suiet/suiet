72336ce3c4fa2e63e8c7c9ef747373dbc44b1988
LLGZONE <985395976@qq.com>
2022-09-04 18:41:15 +0800

    feat: change tns and back

--- packages/chrome/src/pages/SettingsPage/nav.tsx
+++ packages/chrome/src/pages/SettingsPage/nav.tsx
@@ -4,7 +4,8 @@ import './common.scss';
 function Nav() {
   const navigate = useNavigate();
   return (
-    <div className="flex fixed items-center h-14 top-0">
+    <div className="fixed top-0 w-full left-0">
+      <div className="flex items-center h-14 w-full bg-white"></div>
       <div
         className="setting-cancel"
         onClick={() => navigate('/settings')}
