03db008ff22bd67eb1f37bad9f2b749515886eb6
brrruski <sizhe4real@gmail.com>
2022-09-04 11:54:26 -0400

    Dev bruski (#59)
    feat: token list

--- packages/chrome/src/hooks/useCoinBalance.ts
+++ packages/chrome/src/hooks/useCoinBalance.ts
@@ -1,6 +1,7 @@
 import { coreApi } from '@suiet/core';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useSWR from 'swr';
+import { swrLoading } from '../utils/others';
 
 export enum CoinSymbol {
   SUI = 'SUI',
@@ -8,14 +9,18 @@ export enum CoinSymbol {
 
 export function useCoinBalance(
   address: string,
-  symbol: CoinSymbol,
+  symbol?: CoinSymbol,
   opts: {
     networkId?: string;
   } = {}
 ) {
   const [balance, setBalance] = useState<string>('0'); // BigInt -> string
   const { networkId = 'devnet' } = opts;
-  const { data: coinsBalanceMap, error } = useSWR(
+  const {
+    data: coinsBalanceMap,
+    error,
+    isValidating,
+  } = useSWR(
     ['fetchCoinsBalanceMap', address, networkId],
     fetchCoinsBalanceMap
   );
@@ -25,7 +30,7 @@ export function useCoinBalance(
     address: string,
     networkId: string
   ) {
-    const map = new Map<string, { balance: bigint; symbol: string }>();
+    const map = new Map<string, string>();
     if (!address || !networkId) return map;
 
     const network = await coreApi.network.getNetwork(networkId);
@@ -38,20 +43,31 @@ export function useCoinBalance(
       throw new Error(`fetch coinsBalance failed: ${address}, ${networkId}`);
     }
     coinsBalance.forEach((item) => {
-      map.set(item.symbol, item);
+      map.set(item.symbol, String(item.balance));
     });
+
     return map;
   }
 
+  const getBalance = useCallback(
+    (symbol: string): string => {
+      if (!symbol) return '0';
+      return coinsBalanceMap?.get(symbol) ?? '0';
+    },
+    [coinsBalanceMap]
+  );
+
   useEffect(() => {
-    if (!coinsBalanceMap) return;
+    if (!coinsBalanceMap || !symbol) return;
     const result = coinsBalanceMap.get(symbol);
-    setBalance(result?.balance ? String(result?.balance) : '0');
+    setBalance(result ? String(result) : '0');
   }, [coinsBalanceMap, symbol]);
 
   return {
     balance,
     error,
-    loading: !error && !coinsBalanceMap,
+    isValidating,
+    loading: swrLoading(coinsBalanceMap, error),
+    getBalance,
   };
 }
