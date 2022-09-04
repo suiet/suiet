9a298f3403e9c0639871ae96981802ca44c13699
bruceeewong <292803281@qq.com>
2022-09-04 12:46:54 -0400

    feat: send loading

--- packages/chrome/src/components/Loading/index.tsx
+++ packages/chrome/src/components/Loading/index.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import ReactLoading from 'react-loading';
 
-const FullPageLoading = () => {
+export const LoadingSpin = () => {
+  return <ReactLoading type={'spin'} color={'#3EA2F8'}></ReactLoading>;
+};
+export const LoadingSpokes = (props: {
+  color?: string;
+  width?: string;
+  height?: string;
+}) => {
+  return (
+    <ReactLoading type={'spokes'} color={'#3EA2F8'} {...props}></ReactLoading>
+  );
+};
+
+export const FullPageLoading = () => {
   return <div></div>;
 };
 
-export default FullPageLoading;
+export default {
+  Spin: LoadingSpin,
+  Spokes: LoadingSpokes,
+};
