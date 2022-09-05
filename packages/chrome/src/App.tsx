092ede8b54e9a7b3b6b0402d448cdf82e43d47dc
bruceeewong <292803281@qq.com>
2022-09-05 01:06:32 -0400

    feat: add error handling

--- packages/chrome/src/App.tsx
+++ packages/chrome/src/App.tsx
@@ -1,17 +1,36 @@
 import { useRoutes } from 'react-router-dom';
 import './App.css';
-import { ToastContainer } from 'react-toastify';
 import routesConfig from './routes';
 import './styles/react-toastify.scss';
 import 'react-loading-skeleton/dist/skeleton.css';
+import ErrorBoundary from './components/ErrorBoundary';
+import { useEffect } from 'react';
+import message from './components/message';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const routes = useRoutes(routesConfig);
 
+  useEffect(() => {
+    const handleError = (event: PromiseRejectionEvent) => {
+      console.error('catch unhandledrejection:', event);
+      event.promise.catch((e) => {
+        message.error(e.message);
+      });
+      event.preventDefault();
+    };
+    window.addEventListener('unhandledrejection', handleError);
+    return () => {
+      window.removeEventListener('unhandledrejection', handleError);
+    };
+  }, []);
+
   return (
     <div className="app">
-      {routes}
-      <ToastContainer />
+      <ErrorBoundary>
+        {routes}
+        <ToastContainer />
+      </ErrorBoundary>
     </div>
   );
 }
