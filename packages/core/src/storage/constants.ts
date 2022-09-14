9a6c8204522948c3958ec37b99ff77e4b2741c27
bruceeewong <292803281@qq.com>
2022-09-13 22:13:20 -0400

    chore(core): fix circular reference

---
+++ packages/core/src/storage/constants.ts
@@ -0,0 +1,10 @@
+export const GLOBAL_META_ID = 'suiet-meta';
+export const DB_NAME = 'Suiet';
+export const DB_VERSION = 4;
+export const DATA_VERSION = 1; // app data version, update when data structure changed
+
+export enum StoreName {
+  META = 'meta',
+  WALLETS = 'wallets',
+  ACCOUNTS = 'accounts',
+}
