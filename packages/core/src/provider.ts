d6139571b76fea6318f81e927c0a0cd63812499b
5kbpers <hustmh@gmail.com>
2022-10-20 02:44:22 +0800

    feat(api): fix get_transaction response type
    Signed-off-by: 5kbpers <hustmh@gmail.com>

--- packages/core/src/provider.ts
+++ packages/core/src/provider.ts
@@ -162,9 +162,10 @@ export class QueryProvider {
     if (txs.length === 0 || !txs[0]) {
       return [];
     }
-    const digests = txs
-      .map((tx) => tx[1])
-      .filter((value, index, self) => self.indexOf(value) === index);
+    const digests = txs.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+    console.log('digests', digests);
 
     const effects = await this.provider.getTransactionWithEffectsBatch(digests);
     const results = [];
@@ -246,8 +247,10 @@ export class QueryProvider {
               module: moveCall.module,
               function: moveCall.function,
               arguments: moveCall.arguments?.map((arg) => JSON.stringify(arg)),
-              created: await this.getTxObjects(effect.effects.created),
-              mutated: await this.getTxObjects(effect.effects.mutated),
+              created: [],
+              mutated: [],
+              // created: await this.getTxObjects(effect.effects.created),
+              // mutated: await this.getTxObjects(effect.effects.mutated),
             },
           });
         }
