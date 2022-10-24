6a99dfb8848b4d8d9733e54bc85ce5363e89917d
hzy <farawayfed@gmail.com>
2022-10-25 03:47:40 +0800

    feat: auto biometric auth when Session mounted

--- packages/chrome/src/components/Session.tsx
+++ packages/chrome/src/components/Session.tsx
@@ -5,23 +5,30 @@ import { useEffect } from 'react';
 import { useApiClient } from '../hooks/useApiClient';
 import { updateAuthed } from '../store/app-context';
 import LockPage from '../pages/LockPage';
+import { useBiometricAuth } from '../hooks/useBiometricAuth';
 
 const Session = (props: Extendable) => {
   const authed = useSelector((state: RootState) => state.appContext.authed);
   const dispatch = useDispatch();
   const apiClient = useApiClient();
+  const { authenticate } = useBiometricAuth();
 
-  async function verifyAuthStatus() {
+  async function verifyAuthStatus(ac: AbortController) {
     try {
       await apiClient.callFunc('auth.isAuthed', null);
       dispatch(updateAuthed(true));
     } catch (e) {
       dispatch(updateAuthed(false));
+      await authenticate(ac.signal);
     }
   }
 
   useEffect(() => {
-    verifyAuthStatus();
+    const controller = new AbortController();
+    verifyAuthStatus(controller);
+    return () => {
+      controller.abort();
+    };
   }, []);
 
   if (!authed) return <LockPage />;
