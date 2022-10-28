b322f25403e78ee7e3d4bbc623b44b2b833aecf3
hzy <farawayfed@gmail.com>
2022-10-28 23:58:09 +0800

    feat: new biometric auth with rotate api

--- packages/core/src/api/auth.ts
+++ packages/core/src/api/auth.ts
@@ -85,7 +85,7 @@ export class AuthApi implements IAuthApi {
     });
     await this.storage.updateMetaAndWallets(newMeta, wallets);
 
-    await this.resetBiometricData();
+    await this.biometricAuthTokenChanged(newToken.toString('hex'));
     await this.login(newPassword);
   }
 
@@ -115,6 +115,7 @@ export class AuthApi implements IAuthApi {
     const token = await this.loadTokenWithPassword(password);
     await maybeFixDataConsistency(this.storage, token);
     this.session.setToken(token);
+    await this.biometricAuthTokenChanged(token);
   }
 
   public async logout() {
@@ -146,48 +147,149 @@ export class AuthApi implements IAuthApi {
     return meta.clientId;
   }
 
-  public async setBiometricData({
-    credentialIdBase64,
-    publicKeyBase64,
-  }: SetBiometricDataParams) {
-    const token = this.getToken();
-
-    const meta = await this.storage.loadMeta();
-    if (!meta) {
-      throw new Error('Password uninitialized');
-    }
-
-    // if (meta.biometricData) {
-    //   throw new Error('Biometric authentication already set');
-    // }
-
-    const authKey = generateClientId(24);
-    const encryptedToken = crypto.encryptString(Buffer.from(authKey), token);
-
-    meta.biometricData = {
-      credentialIdBase64,
-      publicKeyBase64,
-      encryptedToken,
-    };
-
-    await this.storage.saveMeta(meta);
-
-    return authKey;
-  }
-
-  public async hasBiometricData() {
+  private async biometricAuthTokenChanged(token: string) {
     const meta = await this.storage.loadMeta();
     if (!meta) {
       throw new Error('Password uninitialized');
     }
 
     if (typeof meta.biometricData === 'object' && meta.biometricData !== null) {
-      return true;
+      const clientId = await this.getClientId();
+      const newAuthKey = generateClientId(24);
+      const encryptedToken = crypto.encryptString(
+        Buffer.from(newAuthKey),
+        token
+      );
+
+      try {
+        const resp = await fetch(
+          `https://api.suiet.app/extension/auth/set-auth-key`,
+          {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              client_id: clientId,
+              auth_key: newAuthKey,
+              public_key_base64: meta.biometricData.publicKeyBase64,
+            }),
+          }
+        );
+        if (resp.ok) {
+          meta.biometricData.encryptedToken = encryptedToken;
+          await this.storage.saveMeta(meta);
+        } else {
+          throw new Error('Failed to set auth key');
+        }
+      } catch (e) {
+        delete meta.biometricData;
+        await this.storage.clearMeta();
+        await this.storage.saveMeta(meta);
+      }
     }
+  }
+
+  private async biometricAuthUnlockWithAuthKey(
+    authKey: string,
+    newAuthKey?: string
+  ) {
+    const meta = await this.storage.loadMeta();
+    if (!meta) {
+      throw new Error('Password uninitialized');
+    }
+
+    if (!meta.biometricData) {
+      throw new Error('Biometric authentication not enabled');
+    }
+
+    const token = crypto.decryptString(
+      Buffer.from(authKey),
+      meta.biometricData.encryptedToken
+    );
+    if (!crypto.validateToken(Buffer.from(token, 'hex'), meta.cipher)) {
+      throw new Error('Invalid password');
+    }
+
+    newAuthKey = newAuthKey ?? generateClientId(24);
+    const encryptedToken = crypto.encryptString(Buffer.from(newAuthKey), token);
+
+    meta.biometricData.encryptedToken = encryptedToken;
+    await this.storage.saveMeta(meta);
+    this.session.setToken(token);
+
+    return newAuthKey;
+  }
+
+  public async biometricAuthRotateAuthKey({
+    challengeBase64,
+    signatureBase64,
+  }: any) {
+    const meta = await this.storage.loadMeta();
+    if (!meta) {
+      throw new Error('Password uninitialized');
+    }
+
+    if (!meta.biometricData) {
+      throw new Error('Biometric authentication not enabled');
+    }
+
+    const clientId = await this.getClientId();
+    const newAuthKey = generateClientId(24);
+
+    let shouldClear = false;
+
+    try {
+      const resp = await fetch(
+        `https://api.suiet.app/extension/auth/rotate-auth-key`,
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            client_id: clientId,
+            message_base64: challengeBase64,
+            signature_base64: signatureBase64,
+            public_key_base64: meta.biometricData.publicKeyBase64,
+
+            auth_key: newAuthKey,
+          }),
+        }
+      );
+
+      if (resp.ok) {
+        const { auth_key: authKey } = await resp.json();
+        if (authKey) {
+          try {
+            await this.biometricAuthUnlockWithAuthKey(authKey, newAuthKey);
+            return true;
+          } catch (e) {
+            shouldClear = true;
+          }
+        }
+      } else {
+        try {
+          const { error_code: errorCode } = await resp.json();
+          if (errorCode === 'NOT_FOUND') {
+            shouldClear = true;
+          }
+        } catch (e) {}
+      }
+    } catch (e) {}
+
+    try {
+      if (shouldClear) {
+        delete meta.biometricData;
+        await this.storage.clearMeta();
+        await this.storage.saveMeta(meta);
+      }
+    } catch (e) {}
+
     return false;
   }
 
-  public async getBiometricData() {
+  public async biometricAuthGetData() {
     const meta = await this.storage.loadMeta();
     if (!meta) {
       throw new Error('Password uninitialized');
@@ -202,7 +304,50 @@ export class AuthApi implements IAuthApi {
     }
   }
 
-  public async resetBiometricData() {
+  public async biometricAuthEnable({
+    credentialIdBase64,
+    publicKeyBase64,
+  }: SetBiometricDataParams) {
+    const meta = await this.storage.loadMeta();
+    if (!meta) {
+      throw new Error('Password uninitialized');
+    }
+
+    const token = this.getToken();
+    const clientId = await this.getClientId();
+    const authKey = generateClientId(24);
+    const encryptedToken = crypto.encryptString(Buffer.from(authKey), token);
+
+    try {
+      const resp = await fetch(
+        `https://api.suiet.app/extension/auth/set-auth-key`,
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            client_id: clientId,
+            auth_key: authKey,
+            public_key_base64: publicKeyBase64,
+          }),
+        }
+      );
+
+      if (resp.ok) {
+        meta.biometricData = {
+          credentialIdBase64,
+          publicKeyBase64,
+          encryptedToken,
+        };
+        await this.storage.saveMeta(meta);
+        return true;
+      }
+    } catch (e) {}
+    return false;
+  }
+
+  public async biometricAuthDisable() {
     const meta = await this.storage.loadMeta();
     if (!meta) {
       throw new Error('Password uninitialized');
@@ -212,32 +357,16 @@ export class AuthApi implements IAuthApi {
       delete meta.biometricData;
       await this.storage.clearMeta();
       await this.storage.saveMeta(meta);
+      await fetch(`https://api.suiet.app/extension/auth/delete-auth-key`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          client_id: await this.getClientId(),
+        }),
+      });
     }
-  }
-
-  public async unlockWithAuthKey(authKey: string) {
-    const meta = await this.storage.loadMeta();
-    if (!meta) {
-      throw new Error('Password uninitialized');
-    }
-
-    if (!meta.biometricData) {
-      throw new Error('Biometric authentication not set');
-    }
-
-    const token = crypto.decryptString(
-      Buffer.from(authKey),
-      meta.biometricData.encryptedToken
-    );
-
-    const newAuthKey = generateClientId(24);
-    const encryptedToken = crypto.encryptString(Buffer.from(newAuthKey), token);
-
-    meta.biometricData.encryptedToken = encryptedToken;
-    await this.storage.saveMeta(meta);
-    this.session.setToken(token);
-
-    return newAuthKey;
   }
 }
 
