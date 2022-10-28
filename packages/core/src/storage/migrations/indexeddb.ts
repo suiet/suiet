3024443a1df26cfcea082d8bf76f8063490ae3ec
5kbpers <hustmh@gmail.com>
2022-10-28 12:26:59 +0800

    feat(core): fix data inconsistency caused by migration
    Signed-off-by: 5kbpers <hustmh@gmail.com>

--- packages/core/src/storage/migrations/indexeddb.ts
+++ packages/core/src/storage/migrations/indexeddb.ts
@@ -1,7 +1,6 @@
 import { Account } from '../types';
 import { isNonEmptyArray } from '../../utils';
 import { StoreName } from '../constants';
-import { derivationHdPath } from '../../crypto';
 
 export type MigrationMethod = (
   db: IDBDatabase,
@@ -34,22 +33,6 @@ async function getAccount(
       .transaction([StoreName.ACCOUNTS])
       .objectStore(StoreName.ACCOUNTS)
       .get(accountId);
-
-    request.onsuccess = (event) => {
-      resolve(request.result);
-    };
-    request.onerror = (event) => {
-      reject(event);
-    };
-  });
-}
-
-async function getAccounts(db: IDBDatabase): Promise<Account[]> {
-  return await new Promise((resolve, reject) => {
-    const request = db
-      .transaction([StoreName.ACCOUNTS])
-      .objectStore(StoreName.ACCOUNTS)
-      .getAll();
 
     request.onsuccess = (event) => {
       resolve(request.result);
@@ -132,64 +115,4 @@ const migrateFrom0To1: MigrationMethod = async (db, oldVersion, newVersion) => {
   };
 };
 
-/**
- * version 1 -> 2
- * 1. update account hd path from m/44'/784'/${id}' to m/44'/784'/0'/0'/${id}'
- */
-const migrateFrom1To2: MigrationMethod = async (db, oldVersion, newVersion) => {
-  console.debug('[db migration] >>> version 1 -> 2, started');
-
-  const accounts = await getAccounts(db);
-  console.debug('[db migration] get all accounts', accounts);
-  if (!isNonEmptyArray(accounts)) {
-    console.debug('[db migration] <<< accounts are empty, migration finished');
-    return;
-  }
-
-  const accountListThatNeedsUpgrade: any[] = [];
-  for (const account of accounts) {
-    if (account.hdPath.split('/').length === 4) {
-      const res = account.hdPath.match(/m\/44'\/784'\/(\d+)'/);
-      if (!res) {
-        console.debug(
-          `[db migration] account hd path is invalid. accountId=${account.id}`
-        );
-        continue;
-      }
-      account.hdPath = derivationHdPath(+res[1]);
-      accountListThatNeedsUpgrade.push(account);
-    }
-    if (!isNonEmptyArray(accountListThatNeedsUpgrade)) {
-      console.debug(
-        '[db migration] <<< no account needs to upgrade, migration finished.'
-      );
-      return;
-    }
-
-    console.debug(
-      '[db migration] accounts that needs to upgrade',
-      accountListThatNeedsUpgrade
-    );
-    const accountBulkUpgradeTx = db.transaction(
-      [StoreName.ACCOUNTS],
-      'readwrite'
-    );
-    const accountStore = accountBulkUpgradeTx.objectStore(StoreName.ACCOUNTS);
-    for (const updatedAccount of accountListThatNeedsUpgrade) {
-      accountStore.put(updatedAccount);
-    }
-    accountBulkUpgradeTx.oncomplete = (e) => {
-      console.debug(
-        `[db migration] <<< AccountBulkUpgradeTx succeeded! Welcome to new version ${newVersion} !`
-      );
-    };
-    accountBulkUpgradeTx.onerror = (e) => {
-      console.error('[db migration] <<< AccountBulkUpgradeTx failed!', e);
-    };
-  }
-};
-
-export default new Map<string, MigrationMethod>([
-  ['0->1', migrateFrom0To1],
-  ['1->2', migrateFrom1To2],
-]);
+export default new Map<string, MigrationMethod>([['0->1', migrateFrom0To1]]);
