49c9f96b8779002c5cb306811a72c67e90b727b5
Fred Liang <info@fredliang.cn>
2022-10-30 16:34:50 +0800

    feat(double confirm): finish double confirm page

--- packages/chrome/src/components/secrets/PasswordConfirmModal/index.tsx
+++ packages/chrome/src/components/secrets/PasswordConfirmModal/index.tsx
@@ -14,8 +14,13 @@ import { useApiClient } from '../../../hooks/useApiClient';
 import styles from './index.module.scss';
 import SettingTwoLayout from '../../../layouts/SettingTwoLayout';
 import { ReactComponent as IconError } from '../../../assets/icons/error.svg';
+import { ReactComponent as IconKey } from '../../../assets/icons/key.svg';
+import { ReactComponent as IconShareError } from '../../../assets/icons/share-error.svg';
+import { ReactComponent as IconQuestionError } from '../../../assets/icons/question-error.svg';
 import Icon from '../../Icon';
 import Typo from '../../../components/Typo';
+import classnames from 'classnames';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
 export type PasswordConfirmModalProps = Extendable & {
   trigger: JSX.Element;
   actionDesc: string;
@@ -37,8 +42,12 @@ type FormData = {
 const WarningDescription = (props: WarningDescriptionProps) => {
   return (
     <div className={styles['warning-description']}>
-      <div className={styles['warning-description__icon']}>{props.icon}</div>
-      <div className={styles['warning-description__title']}>{props.title}</div>
+      <div className="flex gap-[8px] items-center">
+        <div className={styles['warning-description__icon']}>{props.icon}</div>
+        <div className={styles['warning-description__title']}>
+          {props.title}
+        </div>
+      </div>
       <div className={styles['warning-description__desc']}>
         {props.description}
       </div>
@@ -76,16 +85,38 @@ const PasswordConfirmModal = (props: PasswordConfirmModalProps) => {
       title={''}
       trigger={props.trigger}
       contentProps={{
-        className: styles['modal'],
+        className: styles['modal'] + ' overflow-scroll h-[100vh] no-scrollbar',
       }}
       onOpenChange={props.onOpenChange}
     >
-      <SettingTwoLayout className={'!p-0'}>
+      <SettingTwoLayout className={classnames('!pb-[128px]', '!p-0')}>
         <div className="flex flex-col items-center gap-[8px]">
           <Icon elClassName={styles['icon']} icon={<IconError />} />
           <Typo.Title className={styles['title']}>Warning</Typo.Title>
         </div>
-
+        <div className="mt-[24px] mb-60px">
+          <WarningDescription
+            icon={<IconKey />}
+            title={'Full control of your wallet'}
+            description={
+              'Anyone with the private key or recovery phrases can have full control of your wallet funds and assets.'
+            }
+          />
+          <WarningDescription
+            icon={<IconShareError />}
+            title={'Never share with anyone'}
+            description={
+              'You should not share your private key or  recovery phrases with anyone, or type in any applications.'
+            }
+          />
+          <WarningDescription
+            icon={<IconQuestionError />}
+            title={'Suiet will never ask for it'}
+            description={
+              'Suiet will never ask for your private key or recovery phrases.'
+            }
+          />
+        </div>
         {/* <Alert type={'warning'} className={'mt-[32px]'}>
           {props.actionDesc}
         </Alert> */}
@@ -104,7 +135,15 @@ const PasswordConfirmModal = (props: PasswordConfirmModalProps) => {
           </Form>
         </div> */}
 
-        <div className={'mt-[16px]'}>
+        <div
+          className={classnames(
+            styles['password-confirm'],
+            'fixed',
+            'bottom-0',
+            'left-0',
+            'w-screen'
+          )}
+        >
           <Form form={form} onSubmit={handleSubmit}>
             <FormControl
               name={'password'}
@@ -117,8 +156,14 @@ const PasswordConfirmModal = (props: PasswordConfirmModalProps) => {
               />
             </FormControl>
 
-            <div className={'flex items-center mt-[16px]'}>
-              <Button type={'submit'} state={'primary'}>
+            <div className={'flex items-center mt-[16px] gap-[8px]'}>
+              <DialogPrimitive.Close className="w-full" aria-label="Close">
+                <Button type={'submit'} solidBackground={true}>
+                  Cancel
+                </Button>
+              </DialogPrimitive.Close>
+
+              <Button type={'submit'} state={'danger'} solidBackground={true}>
                 Confirm
               </Button>
             </div>
