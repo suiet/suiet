a7d836e74ce4ef8a98fe7847e88cc308785c104d
brrruski <sizhe4real@gmail.com>
2022-10-30 11:29:54 -0400

    fix(ext): empower welcome page with initialization adjust effect (#48)

--- packages/chrome/src/components/RequireInit.tsx
+++ packages/chrome/src/components/RequireInit.tsx
@@ -2,24 +2,15 @@ import { useCallback, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
-import { Wallet } from '@suiet/core';
 import { isNonEmptyArray } from '../utils/check';
-import {
-  resetAppContext,
-  updateAccountId,
-  updateInitialized,
-  updateNetworkId,
-  updateWalletId,
-} from '../store/app-context';
-import { useApiClient } from '../hooks/useApiClient';
+import { updateNetworkId } from '../store/app-context';
 import { useFeatureFlags } from '../hooks/useFeatureFlags';
+import { useEffectAdjustInitializedStatus } from '../hooks/useEffectAdjustInitializedStatus';
 
 function RequireInit({ children }: any) {
   const appContext = useSelector((state: RootState) => state.appContext);
   const dispatch = useDispatch<AppDispatch>();
-  const apiClient = useApiClient();
   const featureFlags = useFeatureFlags();
-
   const adjustCurrentNetworkId = useCallback(async () => {
     const defaultNetwork = featureFlags?.default_network ?? 'devnet';
 
@@ -38,42 +29,7 @@ function RequireInit({ children }: any) {
     }
   }, [featureFlags, appContext]);
 
-  async function adjustInitializedStatus() {
-    const wallets = await apiClient.callFunc<null, Wallet[]>(
-      'wallet.getWallets',
-      null
-    );
-    if (!isNonEmptyArray(wallets)) {
-      if (appContext.initialized) {
-        // no wallets, reset app
-        await dispatch(resetAppContext());
-      }
-      return;
-    }
-
-    const [firstWallet] = wallets;
-    if (!firstWallet?.id || !isNonEmptyArray(firstWallet?.accounts)) {
-      // wallet account missing, data may be messed up, reset app
-      await dispatch(resetAppContext());
-      return;
-    }
-
-    const [firstAccount] = firstWallet.accounts;
-    // if wallet data is correct, but context data is not, re-initialize app
-    if (!appContext.initialized) {
-      // if db has data but context is incorrect, then update
-      await dispatch(updateInitialized(true));
-      await dispatch(updateWalletId(firstWallet.id));
-      await dispatch(updateAccountId(firstAccount.id));
-      await dispatch(updateNetworkId('devnet'));
-    }
-  }
-
-  // adjust init status by checking the wallets data
-  useEffect(() => {
-    adjustInitializedStatus();
-  }, []);
-
+  useEffectAdjustInitializedStatus(appContext);
   useEffect(() => {
     adjustCurrentNetworkId();
   }, [appContext.networkId, featureFlags]);
---
+++ packages/chrome/src/hooks/useEffectAdjustInitializedStatus.ts
@@ -0,0 +1,63 @@
+import { Wallet } from '@suiet/core';
+import { isNonEmptyArray } from '../utils/check';
+import {
+  AppContextState,
+  resetAppContext,
+  updateAccountId,
+  updateInitialized,
+  updateNetworkId,
+  updateWalletId,
+} from '../store/app-context';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../store';
+import { useApiClient } from './useApiClient';
+
+/**
+ * Detect indexeddb and redux state, make adjustment if there exists any mismatch
+ * @param appContext
+ */
+export function useEffectAdjustInitializedStatus(appContext: AppContextState) {
+  const dispatch = useDispatch<AppDispatch>();
+  const apiClient = useApiClient();
+
+  async function adjustInitializedStatus() {
+    const wallets = await apiClient.callFunc<null, Wallet[]>(
+      'wallet.getWallets',
+      null
+    );
+    if (!isNonEmptyArray(wallets)) {
+      // if no wallet
+      if (appContext.initialized) {
+        console.warn(
+          'no wallets detected under initialized context, reset app'
+        );
+        await dispatch(resetAppContext());
+      }
+      return;
+    }
+
+    const [firstWallet] = wallets;
+    if (!firstWallet?.id || !isNonEmptyArray(firstWallet?.accounts)) {
+      console.warn('wallet account missing, data may be messed up, reset app');
+      await dispatch(resetAppContext());
+      return;
+    }
+
+    const [firstAccount] = firstWallet.accounts;
+    // if wallet data is correct, but context data is not, re-initialize app
+    if (!appContext.initialized) {
+      console.log('wallets detected, re-initialize app');
+      // if db has data but context is incorrect, then update
+      await dispatch(updateInitialized(true));
+      await dispatch(updateWalletId(firstWallet.id));
+      await dispatch(updateAccountId(firstAccount.id));
+      await dispatch(updateNetworkId('devnet'));
+    }
+  }
+
+  // adjust init status by checking the wallets data
+  useEffect(() => {
+    adjustInitializedStatus();
+  }, []);
+}
