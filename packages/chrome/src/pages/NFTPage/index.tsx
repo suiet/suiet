5352c7c86aab359237da819bf61766b2805af107
Fred Liang <info@fredliang.cn>
2022-11-01 02:01:38 +0800

    feat: add mint button

--- packages/chrome/src/pages/NFTPage/index.tsx
+++ packages/chrome/src/pages/NFTPage/index.tsx
@@ -1,14 +1,23 @@
 import NftList from '../NFTPage/NftList';
 import Typo from '../../components/Typo';
 import styles from './index.module.scss';
+import message from '../../components/message';
+import Button from '../../components/Button';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { useAccount } from '../../hooks/useAccount';
 import { useNftList } from '../../hooks/useNftList';
 import { isNonEmptyArray } from '../../utils/check';
 import Empty from './NftList/Empty';
+import { MintNftParams } from '@suiet/core';
 import { sleep } from '../../utils/time';
 import AppLayout from '../../layouts/AppLayout';
+import { CoinSymbol, useCoinBalance } from '../../hooks/useCoinBalance';
+import { useApiClient } from '../../hooks/useApiClient';
+import { OmitToken } from '../../types';
+import { useNetwork } from '../../hooks/useNetwork';
+import { useState } from 'react';
+import { ReactComponent as GiftIcon } from '../../assets/icons/gift.svg';
 
 function MainPage() {
   const appContext = useSelector((state: RootState) => state.appContext);
@@ -18,6 +27,43 @@ function MainPage() {
     loading,
     mutate: refreshNftList,
   } = useNftList(account?.address ?? '', appContext.networkId);
+
+  const apiClient = useApiClient();
+  const { data: network } = useNetwork(appContext.networkId);
+  const [sendLoading, setSendLoading] = useState(false);
+
+  const { balance, loading: balanceLoading } = useCoinBalance(
+    CoinSymbol.SUI,
+    account?.address ?? '',
+    appContext.networkId
+  );
+
+  async function mintSampleNFT() {
+    // example address: ECF53CE22D1B2FB588573924057E9ADDAD1D8385
+    if (!network) throw new Error('require network selected');
+
+    if (balanceLoading || Number(balance) < 10000) {
+      message.error('Please ensure you have more than 0.00001 SUI to mint');
+      return;
+    }
+    setSendLoading(true);
+    try {
+      await apiClient.callFunc<OmitToken<MintNftParams>, undefined>(
+        'txn.mintExampleNft',
+        {
+          network,
+          walletId: appContext.walletId,
+          accountId: appContext.accountId,
+        },
+        { withAuth: true }
+      );
+      message.success('Mint NFT succeeded');
+    } catch (e: any) {
+      message.error(`Mint NFT failed: ${e?.message}`);
+    } finally {
+      setSendLoading(false);
+    }
+  }
 
   function renderContent() {
     if (!loading && !isNonEmptyArray(nftList))
@@ -35,8 +81,29 @@ function MainPage() {
     return (
       <div className={styles['container']}>
         <header className={styles['header']}>
-          <Typo.Title className={styles['title']}>NFT</Typo.Title>
-          <Typo.Small className={styles['desc']}>Manage your NFTs.</Typo.Small>
+          <div className="flex items-center justify-between">
+            <div className="">
+              <Typo.Title className={styles['title']}>NFT</Typo.Title>
+              <Typo.Small className={styles['desc']}>
+                Manage your NFTs.
+              </Typo.Small>
+            </div>
+            {network?.name !== 'mainnet' && (
+              <Button
+                className="rounded-full w-fit"
+                onClick={mintSampleNFT}
+                loading={sendLoading}
+              >
+                {!sendLoading && (
+                  <GiftIcon
+                    style={{
+                      stroke: '#B9C0D4',
+                    }}
+                  />
+                )}
+              </Button>
+            )}
+          </div>
         </header>
         {loading ? (
           // skeleton
