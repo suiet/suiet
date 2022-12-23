5d0a7a9c85a811ef17633f9b254c533af8773b65
brrruski <sizhe4real@gmail.com>
2022-12-23 10:07:52 -0500

    fix(ext): fix minting nft function of empty page (#69)

--- packages/chrome/src/pages/NFTPage/hooks/useMintNftCampaign.ts
+++ packages/chrome/src/pages/NFTPage/hooks/useMintNftCampaign.ts
@@ -5,21 +5,21 @@ import {
   getMintNftCampaignInfo,
 } from '../../../utils/campaign';
 
+export const defaultNftMetadata = {
+  name: 'Suiet NFT',
+  desc: 'An NFT created by Suiet',
+  imageUrl:
+    'https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4',
+};
+
 export function useMintNftCampaign() {
   const featureFlags = useFeatureFlags();
 
-  const defaultInfo = {
-    name: 'Suiet NFT',
-    desc: 'An NFT created by Suiet',
-    imageUrl:
-      'https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4',
-  };
-
   return useMemo(() => {
-    if (!featureFlags) return defaultInfo;
+    if (!featureFlags) return defaultNftMetadata;
     const campaignInfo = getCampaignInfo(featureFlags);
     const res = getMintNftCampaignInfo(campaignInfo);
-    if (!res) return defaultInfo;
+    if (!res) return defaultNftMetadata;
     return {
       name: res.mint_name,
       desc: res.mint_description,
