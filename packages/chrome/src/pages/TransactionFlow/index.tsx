b02523a59cce6d5c0fddb6e1e05d0e3305b969be
brrruski <sizhe4real@gmail.com>
2023-03-29 11:52:22 -0400

    refactor(ext): remove pull refresh feature for tx history list

--- packages/chrome/src/pages/TransactionFlow/index.tsx
+++ packages/chrome/src/pages/TransactionFlow/index.tsx
@@ -23,13 +23,11 @@ import orderTimeList from './utils/orderTimeList';
 
 function TransactionFlow({
   txHistoryList,
-  refetch,
   fetchMore,
   hasMore,
 }: {
   txHistoryList: TransactionForHistory[];
   address: string;
-  refetch: () => void;
   fetchMore: () => void;
   hasMore: boolean;
 }) {
@@ -49,11 +47,6 @@ function TransactionFlow({
       <InfiniteScroll
         className={'no-scrollbar'}
         dataLength={txHistoryList.length}
-        pullDownToRefresh={true}
-        pullDownToRefreshContent={
-          <Typo.Hints className={'text-center my-1'}>Refreshing...</Typo.Hints>
-        }
-        refreshFunction={refetch}
         next={fetchMore}
         hasMore={false}
         height={390}
@@ -64,32 +57,6 @@ function TransactionFlow({
           </Typo.Hints>
         }
         scrollThreshold={0.8}
-        // refreshFunction={() => {
-        //   client.refetchQueries({
-        //     include: [QUERY_EVENTS],
-        //   });
-        //   message.config({
-        //     top: 100,
-        //   });
-
-        //   messageApi.open({
-        //     type: 'success',
-        //     content: 'refreshed',
-        //     top: 100,
-        //     style: {
-        //       // marginTop: '3.5rem',
-        //       // transform: 'translate(0,3.5rem)'
-        //     },
-        //   });
-        // }}
-        // pullDownToRefresh
-        // pullDownToRefreshThreshold={50}
-        // pullDownToRefreshContent={
-        //   <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
-        // }
-        // releaseToRefreshContent={
-        //   <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
-        // }
       >
         {days.map((day) => {
           return (
@@ -189,7 +156,6 @@ function TransactionPage() {
         <TransactionFlow
           txHistoryList={transactions}
           address={address ?? ''}
-          refetch={refetch}
           fetchMore={fetchMore}
           hasMore={hasMore}
         />
