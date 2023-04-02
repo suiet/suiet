60e7ca1035934a52c4ad0155514121c8bf7676bd
brrruski <sizhe4real@gmail.com>
2023-04-01 22:48:00 -0400

    ci(ext): add global test npm script

--- packages/chrome/jest.config.ts
+++ packages/chrome/jest.config.ts
@@ -106,7 +106,7 @@ export default {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ['jest-simple-dot-reporter', 'default'],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
