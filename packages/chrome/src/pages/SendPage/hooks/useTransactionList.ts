956af4601cdba1c64cf73571a42e38cc0b5a1dde
lean <985395976@qq.com>
2023-04-04 00:06:28 +0800

    fix: fix sendpage trasaction nums (#129)

--- packages/chrome/src/pages/SendPage/hooks/useTransactionList.ts
+++ packages/chrome/src/pages/SendPage/hooks/useTransactionList.ts
@@ -1,65 +1,67 @@
 import { gql, useLazyQuery } from '@apollo/client';
 import {
-  GetTransactionsParams,
-  TransactionsData,
+  TransactionsFilter,
+  TransactionsResult,
 } from '../../../types/gql/transactions';
 
 const transactionsGql = gql`
   query Transactions(
-    $filter: TransactionsFilter
-    $cursor: TransactionDigest
-    $order: OrderDirection
-    $limit: Int
+    $fromAddress: Address
+    $toAddress: Address
+    $startTime: Timestamp
+    $endTime: Timestamp
   ) {
-    transactions(
-      filter: $filter
-      cursor: $cursor
-      order: $order
-      limit: $limit
+    fromTransactions: transactions(
+      filter: {
+        fromAddress: $fromAddress
+        startTime: $startTime
+        endTime: $endTime
+      }
     ) {
       transactions {
-        digest
-        category
         status
-        gas {
-          computationCost
-          storageRebate
-          storageCost
-        }
-        gasFee
-        epoch
-        signature
-        timestamp
       }
-      nextCursor
+    }
+    toTransactions: transactions(
+      filter: {
+        toAddress: $toAddress
+        startTime: $startTime
+        endTime: $endTime
+      }
+    ) {
+      transactions {
+        status
+      }
     }
   }
 `;
 
 interface Transaction {
-  digest: string;
-  category: string;
-  kind: string;
   status: string;
-  gasFee: number;
-  epoch: number;
-  signature: string;
-  timestamp: number;
 }
 
-function useTransactionList(params: GetTransactionsParams = {}) {
+type Params = Omit<TransactionsFilter, 'mutatedObjectID' | 'inputObjectID'>;
+
+function useTransactionList() {
   const [getTransactionList, { data, loading, error }] = useLazyQuery<
-    TransactionsData<Transaction>,
-    GetTransactionsParams
-  >(transactionsGql, {
-    variables: {
-      ...params,
+    {
+      fromTransactions: TransactionsResult<Transaction>;
+      toTransactions: TransactionsResult<Transaction>;
     },
-  });
+    Params
+  >(transactionsGql, {});
+
+  let res: Transaction[] | null = null;
+
+  if (data) {
+    res = [];
+    res = res.concat(data.fromTransactions.transactions || []);
+    res = res.concat(data.toTransactions.transactions || []);
+  }
 
   return {
     getTransactionList,
-    data: data?.transactions,
+    data: res,
     loading,
     error,
   };
