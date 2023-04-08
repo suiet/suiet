d984ade30c0d03ff44d625f733a7d31a2862274e
brrruski <sizhe4real@gmail.com>
2023-04-08 00:34:06 -0400

    perf(ext): improve apollo cache logic and speed of the cold starting phase

--- packages/chrome/src/store/storage.ts
+++ packages/chrome/src/store/storage.ts
@@ -2,11 +2,11 @@ interface WebStorage {
   /**
    * @desc Fetches key and returns item in a promise.
    */
-  getItem: (key: string) => Promise<string | undefined>;
+  getItem: (key: string) => Promise<string | null>;
   /**
    * @desc Sets value for key and returns item in a promise.
    */
-  setItem: (key: string, item: string) => Promise<void>;
+  setItem: (key: string, item: string | null) => Promise<void>;
   /**
    * @desc Removes value for key.
    */
@@ -14,16 +14,16 @@ interface WebStorage {
 }
 
 export class ChromeStorage implements WebStorage {
-  async getItem(key: string): Promise<string | undefined> {
+  async getItem(key: string) {
     const results = await chrome.storage.local.get(key);
-    return results[key];
+    return results[key] ?? null;
   }
 
   async removeItem(key: string): Promise<void> {
     return await chrome.storage.local.remove(key);
   }
 
-  async setItem(key: string, item: string): Promise<void> {
+  async setItem(key: string, item: string | null): Promise<void> {
     return await chrome.storage.local.set({ [key]: item });
   }
 
