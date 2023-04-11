05c80fe5c4065431e90aed23ec2d02d75d6317ed
brrruski <sizhe4real@gmail.com>
2023-04-11 16:11:30 -0400

    feat: wrap stake tx factory

--- packages/expo/src/components/Airdrop.tsx
+++ packages/expo/src/components/Airdrop.tsx
@@ -62,7 +62,7 @@ export const Airdrop: React.FC<{ recipient: string }> = ({ recipient }) => {
               // message.success('Airdrop succeeded');
               Toast.show({
                 type: 'success',
-                text1: 'Airdrop succeeded!',
+                text1: 'Faucet succeeded!',
                 visibilityTime: 6000,
                 props: {
                   beautifulBorder: true,
@@ -121,7 +121,7 @@ export const Airdrop: React.FC<{ recipient: string }> = ({ recipient }) => {
             // message.error(err.message);
             Toast.show({
               type: 'error',
-              text1: `Failed to Airdrop: Unknown error`,
+              text1: `Failed to Faucet: Unknown error`,
               visibilityTime: 6000,
               props: {
                 icon: require('@assets/red_exclamation_mark.png'),
