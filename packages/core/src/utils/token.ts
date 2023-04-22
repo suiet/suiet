2762274fefdaac02725d1ae4bdcf1f76dd19cc3b
brrruski <sizhe4real@gmail.com>
2023-04-22 16:12:43 -0400

    chore(ext): add token missing warning

--- packages/core/src/utils/token.ts
+++ packages/core/src/utils/token.ts
@@ -3,11 +3,14 @@ import { Storage } from '../storage/Storage';
 import { Buffer } from 'buffer';
 
 export async function validateToken(storage: Storage, token: string) {
+  if (!token) {
+    throw new Error('Auth token is not provided');
+  }
   const meta = await storage.loadMeta();
   if (!meta) {
-    throw new Error('Meta not initialized');
+    throw new Error('Meta is not initialized');
   }
   if (!crypto.validateToken(Buffer.from(token, 'hex'), meta.cipher)) {
-    throw new Error('Invalid token');
+    throw new Error('Invalid auth token');
   }
 }
