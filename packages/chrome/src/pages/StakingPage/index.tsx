c9c3f26b6d8e30098dc7ed83726017b88fd343e6
brrruski <sizhe4real@gmail.com>
2023-04-25 15:06:00 -0400

    refactor(ext): adapt staking page to the inputAmount component

--- packages/chrome/src/pages/StakingPage/index.tsx
+++ packages/chrome/src/pages/StakingPage/index.tsx
@@ -8,9 +8,12 @@ import {
   SendAndExecuteTxParams,
   TxEssentials,
   formatSUI,
+  calculateCoinAmount,
+  maxCoinAmountWithDecimal,
+  isCoinAmountValid,
 } from '@suiet/core';
 import { useNetwork } from '../../hooks/useNetwork';
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { RootState } from '../../store';
 import { useSelector } from 'react-redux';
 import { useQuery } from '@apollo/client';
@@ -25,13 +28,14 @@ import { useFeatureFlagsWithNetwork } from '../../hooks/useFeatureFlags';
 import Skeleton from 'react-loading-skeleton';
 import { createStakeTransaction } from './utils';
 import useSuiBalance from '../../hooks/coin/useSuiBalance';
-import { bigint } from 'superstruct';
+import { SUI_TYPE_ARG } from '@mysten/sui.js';
+import Message from '../../components/message';
+import { compareCoinAmount } from '../../utils/check';
+
 export default function StackingPage() {
   const apiClient = useApiClient();
   const appContext = useSelector((state: RootState) => state.appContext);
   const { data: network } = useNetwork(appContext.networkId);
-  const { walletId } = useSelector((state: RootState) => state.appContext);
-
   const [selectedValidator, setSelectedValidator] = useState<string>();
   const { loading, error, data } = useQuery(GET_VALIDATORS, {
     fetchPolicy: 'cache-and-network',
@@ -40,7 +44,6 @@ export default function StackingPage() {
   useEffect(() => {
     setSelectedValidator(validators[0]?.suiAddress);
   }, [validators]);
-  const gasBudget = network?.stakeGasBudget ?? 10000;
   const currentValidator = validators.find((validator) => {
     if (validator.suiAddress === selectedValidator) {
       return true;
@@ -49,6 +52,7 @@ export default function StackingPage() {
   });
   const [amount, setAmount] = useState('0');
   const [buttonLoading, setButtonLoading] = useState(false);
+
   // const { data: estimatedGasBudget, isSuccess: isBudgetLoaded } =
   //   useEstimatedGasBudget({
   //     kind: 'moveCall',
@@ -65,15 +69,32 @@ export default function StackingPage() {
   //       ],
   //     },
   //   });
+
   const { address } = useAccount(appContext.accountId);
   const { data: suiBalance, loading: balanceLoading } = useSuiBalance(
     address ?? ''
   );
   const featureFlags = useFeatureFlagsWithNetwork();
-  const gasFee = featureFlags?.stake_gas_budget ?? 20_000_000;
-  const max = useMemo(() => {
-    // return Number(formatCurrency(Number(balance) - Number(estimatedGasBudget)));
-    return suiBalance.balance;
+  const gasBudget = featureFlags?.stake_gas_budget ?? 20_000_000;
+
+  const maxAmount = useMemo(() => {
+    return maxCoinAmountWithDecimal(SUI_TYPE_ARG, suiBalance.balance, 9, {
+      gasBudget: String(gasBudget),
+    });
+  }, [suiBalance, gasBudget]);
+
+  const isInputValid = useMemo(() => {
+    return isCoinAmountValid(amount, maxAmount);
+  }, [amount, maxAmount]);
+
+  const showTipOnce = useRef(false);
+  useEffect(() => {
+    if (!suiBalance || showTipOnce.current) return;
+
+    if (compareCoinAmount(suiBalance.balance, gasBudget) < 0) {
+      Message.info('Current balance is not enough to pay the gas fee');
+      showTipOnce.current = true;
+    }
   }, [suiBalance]);
 
   async function StakeCoins() {
@@ -86,7 +107,11 @@ export default function StackingPage() {
       if (!network) throw new Error('require network selected');
       if (!selectedValidator) throw new Error('require validator selected');
 
-      const tx = createStakeTransaction(BigInt(amount), selectedValidator);
+      const stakeSUIAmount = calculateCoinAmount(amount, 9);
+      const tx = createStakeTransaction(
+        BigInt(stakeSUIAmount),
+        selectedValidator
+      );
       await apiClient.callFunc<
         SendAndExecuteTxParams<string, OmitToken<TxEssentials>>,
         undefined
@@ -160,11 +185,12 @@ export default function StackingPage() {
         <InputAmount
           className="h-48"
           onInput={(value) => {
-            setAmount(BigInt(Number(value) * 10 ** suiBalance.decimals));
+            setAmount(value);
           }}
-          maxCoinAmount={max}
+          maxCoinAmount={maxAmount}
           decimals={suiBalance.decimals}
           coinSymbol={'SUI'}
+          isValid={amount === '0' || isInputValid}
         ></InputAmount>
         {/* <div className="font-bold text-zinc-300">SUI</div>
         <button className="text-lg py-1 px-3 bg-zinc-100 text-zinc-500 hover:bg-zinc-200 transition font-medium rounded-2xl">
@@ -184,9 +210,16 @@ export default function StackingPage() {
                 Epoch #{currentValidator?.epoch}
               </div>
             </div>
+
+            <div className="flex flex-row items-center justify-between">
+              <div className="text-zinc-700">SUI Balance</div>
+              <div className="text-zinc-400">
+                {formatSUI(suiBalance.balance)} SUI
+              </div>
+            </div>
             <div className="flex flex-row items-center justify-between">
               <div className="text-zinc-700">Gas Budget</div>
-              <div className="text-zinc-400">{formatSUI(gasFee)} SUI</div>
+              <div className="text-zinc-400">{formatSUI(gasBudget)} SUI</div>
             </div>
           </div>
         </div>
