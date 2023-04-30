060a9149e0e573f087ffb984be330142d530f475
Fred Liang <info@fredliang.cn>
2023-04-30 17:38:50 +0800

    fix: fix mainnet support

--- packages/expo/src/utils/api.ts
+++ packages/expo/src/utils/api.ts
@@ -24,28 +24,6 @@ export interface FeatureFlagRes {
   campaign: Record<string, any>;
 }
 
-// const mockData: FeatureFlagRes = {
-//   available_networks: ['devnet'],
-//   default_network: 'devnet',
-//   networks: {
-//     devnet: {
-//       explorer_url: 'https://explorer.devnet.sui.io',
-//       faucet_api: 'https://faucet.devnet.sui.io',
-//       full_node_url: 'https://fullnode.devnet.suiet.app',
-//       on_maintenance: false,
-//       rpc_version: '0.12.2',
-//     },
-//     testnet: {
-//       explorer_url: 'https://explorer.testnet.sui.io',
-//       faucet_api: 'https://faucet.testnet.sui.io',
-//       full_node_url: 'https://fullnode.testnet.sui.io',
-//       on_maintenance: false,
-//       rpc_version: '0.12.2',
-//     },
-//   },
-//   require_update: false,
-// };
-
 export async function getFeatureFlags(): Promise<FeatureFlagRes> {
   return await suietHttp({
     url: '/feature-flag',
