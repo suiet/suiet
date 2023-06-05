68308c4f362fa83e580729f49762b3c70bff9f49
Fred Liang <info@fredliang.cn>
2023-06-06 01:22:14 +0800

    feat: support resolve SuiNS

--- packages/chrome/src/pages/SendPage/AddressInput/index.tsx
+++ packages/chrome/src/pages/SendPage/AddressInput/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import AddressInput from '../../../components/AddressInput';
 import Button from '../../../components/Button';
@@ -15,6 +15,10 @@ import dayjs from 'dayjs';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import Skeleton from 'react-loading-skeleton';
+import { resolveDomain } from '../../../api/suins';
+import { useDebounceEffect } from 'ahooks';
+import { isValidDomain } from '../../../utils/address';
+import { isValidSuiAddress } from '@mysten/sui.js';
 interface AddressInputValues {
   address: string;
 }
@@ -36,34 +40,57 @@ function AddressInputPage({
   });
   const { networkId } = useSelector((state: RootState) => state.appContext);
   const addressState = getInputStateByFormState(form.formState, 'address');
+
+  const [namerServiceAddress, setNameServiceAddress] = useState('');
+  const [isLoadingNameServiceAddress, setIsLoadingNameServiceAddress] =
+    useState(false);
+
   const formAddress = form.getValues().address;
+
+  const resolvedAddress = namerServiceAddress || formAddress;
+  const isValidateInput = isValidSuiAddress(resolvedAddress);
   const { getTransactionList, data: history, loading } = useTransactionList();
   useWatch({ name: 'address', control: form.control });
+
+  useDebounceEffect(() => {
+    if (isValidDomain(formAddress)) {
+      setIsLoadingNameServiceAddress(true);
+      resolveDomain(formAddress, { networkId })
+        .then((address) => {
+          setNameServiceAddress(address);
+          setIsLoadingNameServiceAddress(false);
+        })
+        .finally(() => setIsLoadingNameServiceAddress(false));
+    } else {
+      setNameServiceAddress('');
+    }
+  }, [formAddress]);
+
   // const loading = true;
   const disabled =
     addressState === 'error' ||
     (state.recipientAddress === '' && addressState === 'default');
 
   useEffect(() => {
-    if (formAddress) {
+    if (isValidSuiAddress(resolvedAddress)) {
       getTransactionList({
         variables: {
-          fromAddress: formAddress,
-          toAddress: formAddress,
+          fromAddress: resolvedAddress,
+          toAddress: resolvedAddress,
           startTime: dayjs().subtract(7, 'd').valueOf(),
           endTime: dayjs().valueOf(),
         },
       });
     }
-  }, [formAddress]);
+  }, [resolvedAddress]);
 
   return (
     <>
       <Form
         form={form}
         onSubmit={(data) => {
-          onNext && onNext();
-          onSubmit(data.address);
+          onNext?.();
+          onSubmit(resolvedAddress);
         }}
       >
         <div className={'px-[32px]'}>
@@ -71,21 +98,43 @@ function AddressInputPage({
             Input Address
           </Typo.Title>
           <Typo.Normal className={`mt-[8px] ${styles['desc']}`}>
-            Enter an validate address
+            Enter an validate address or domain
           </Typo.Normal>
 
-          <AddressInput form={form} className={'mt-[48px]'} />
+          <AddressInput form={form} className={'mt-[24px]'} />
+
+          {isLoadingNameServiceAddress ||
+          (isValidDomain(formAddress) &&
+            !isLoadingNameServiceAddress &&
+            namerServiceAddress) ? (
+            <div className="mt-4 p-2  px-3 rounded-lg bg-slate-100 border border-gray-200 break-words text-gray-500">
+              {isLoadingNameServiceAddress ? (
+                <p>Loading name service address</p>
+              ) : (
+                <p> {namerServiceAddress}</p>
+              )}
+            </div>
+          ) : null}
+
+          {!isLoadingNameServiceAddress &&
+            isValidDomain(formAddress) &&
+            !namerServiceAddress && (
+              <div className="mt-4 p-2 px-3 rounded-lg bg-orange-100 border border-orange-200 break-words text-orange-500">
+                <p>Invalid domain name</p>
+              </div>
+            )}
           {loading ? (
             <Skeleton className="h-[28px] mt-4"></Skeleton>
           ) : (
             history &&
+            isValidateInput &&
             (history.length > 0 ? (
               <div className={styles['transaction-num']}>
                 {history?.length} transactions in a week
                 <div
                   onClick={() => {
                     window.open(
-                      `https://explorer.sui.io/address/${formAddress}?network=${networkId}`,
+                      `https://explorer.sui.io/address/${resolvedAddress}?network=${networkId}`,
                       '_blank'
                     );
                   }}
