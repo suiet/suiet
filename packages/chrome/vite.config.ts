d064dd9ce9d8865b1bac1bb1e0abde2e71fbe119
Fred Liang <info@fredliang.cn>
2023-06-14 11:21:05 +0800

    feat: bump version

--- packages/chrome/vite.config.ts
+++ packages/chrome/vite.config.ts
@@ -4,7 +4,6 @@ import { crx } from '@crxjs/vite-plugin';
 // @ts-ignore
 import manifest from './src/manifest';
 import viteSvgr from 'vite-plugin-svgr';
-import { Buffer } from 'buffer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
