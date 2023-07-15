4594e4dfbefb49b87be5c5710ba14c3641c3b1b4
Fred Liang <info@fredliang.cn>
2023-07-16 00:23:28 +0800

    feat: support select token

--- packages/chrome/src/pages/SwapPage/selectSwapToken.tsx
+++ packages/chrome/src/pages/SwapPage/selectSwapToken.tsx
@@ -7,83 +7,48 @@ import {
   ChevronUpIcon,
 } from '@radix-ui/react-icons';
 import { Extendable } from '../../types';
+
 type SelectProps = Extendable & {
   defaultValue: string;
 };
 
-export const Select = () => (
-  <SelectCompoment.Root>
-    <SelectCompoment.Trigger className="SelectTrigger" aria-label="Food">
-      <SelectCompoment.Value placeholder="Select a fruit…" />
-      <SelectCompoment.Icon className="SelectIcon">
-        <ChevronDownIcon />
-      </SelectCompoment.Icon>
-    </SelectCompoment.Trigger>
-    <SelectCompoment.Portal>
-      <SelectCompoment.Content className="SelectContent">
-        <SelectCompoment.ScrollUpButton className="SelectScrollButton">
-          <ChevronUpIcon />
-        </SelectCompoment.ScrollUpButton>
-        <SelectCompoment.Viewport className="SelectViewport">
-          <SelectCompoment.Group>
-            <SelectCompoment.Label className="SelectLabel">
-              Fruits
-            </SelectCompoment.Label>
-            <SelectItem value="apple">Apple</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-            <SelectItem value="blueberry">Blueberry</SelectItem>
-            <SelectItem value="grapes">Grapes</SelectItem>
-            <SelectItem value="pineapple">Pineapple</SelectItem>
-          </SelectCompoment.Group>
-
-          <SelectCompoment.Separator className="SelectSeparator" />
-
-          <SelectCompoment.Group>
-            <SelectCompoment.Label className="SelectLabel">
-              Vegetables
-            </SelectCompoment.Label>
-            <SelectItem value="aubergine">Aubergine</SelectItem>
-            <SelectItem value="broccoli">Broccoli</SelectItem>
-            <SelectItem value="carrot" disabled>
-              Carrot
-            </SelectItem>
-            <SelectItem value="courgette">Courgette</SelectItem>
-            <SelectItem value="leek">Leek</SelectItem>
-          </SelectCompoment.Group>
-
-          <SelectCompoment.Separator className="SelectSeparator" />
-
-          <SelectCompoment.Group>
-            <SelectCompoment.Label className="SelectLabel">
-              Meat
-            </SelectCompoment.Label>
-            <SelectItem value="beef">Beef</SelectItem>
-            <SelectItem value="chicken">Chicken</SelectItem>
-            <SelectItem value="lamb">Lamb</SelectItem>
-            <SelectItem value="pork">Pork</SelectItem>
-          </SelectCompoment.Group>
-        </SelectCompoment.Viewport>
-        <SelectCompoment.ScrollDownButton className="SelectScrollButton">
+export function Select(props: SelectProps) {
+  return (
+    <SelectCompoment.Root defaultValue={props.defaultValue}>
+      <SelectCompoment.Trigger className="flex items-center" aria-label="Food">
+        <SelectCompoment.Value placeholder="Select a coin" />
+        <SelectCompoment.Icon className="SelectIcon">
           <ChevronDownIcon />
-        </SelectCompoment.ScrollDownButton>
-      </SelectCompoment.Content>
-    </SelectCompoment.Portal>
-  </SelectCompoment.Root>
-);
+        </SelectCompoment.Icon>
+      </SelectCompoment.Trigger>
+      <SelectCompoment.Portal className="fixed bottom-0 left-0 right-0 bg-white w-full p-4 pb-8 shadow-lg">
+        <SelectCompoment.Content className="SelectContent">
+          <h2 className="font-xl font-medium mx-auto"> Select a Coin </h2>
+          <SelectCompoment.ScrollUpButton className="SelectScrollButton">
+            <ChevronUpIcon />
+          </SelectCompoment.ScrollUpButton>
+          <SelectCompoment.Viewport className="SelectViewport">
+            {props.children}
+          </SelectCompoment.Viewport>
+          <SelectCompoment.ScrollDownButton className="SelectScrollButton">
+            <ChevronDownIcon />
+          </SelectCompoment.ScrollDownButton>
+        </SelectCompoment.Content>
+      </SelectCompoment.Portal>
+    </SelectCompoment.Root>
+  );
+}
 
-export const SelectItem = React.forwardRef(
-  ({ children, className, ...props }, forwardedRef) => {
-    return (
-      <SelectCompoment.Item
-        className={classnames('SelectItem', className)}
-        {...props}
-        ref={forwardedRef}
-      >
-        <SelectCompoment.ItemText>{children}</SelectCompoment.ItemText>
-        <SelectCompoment.ItemIndicator className="SelectItemIndicator">
-          <CheckIcon />
-        </SelectCompoment.ItemIndicator>
-      </SelectCompoment.Item>
-    );
-  }
-);
+type SelectItemProps = Extendable & {
+  value: string;
+};
+export function SelectItem(props: SelectItemProps) {
+  return (
+    <SelectCompoment.Item
+      className={classnames('SelectItem', props.className)}
+      {...props}
+    >
+      <SelectCompoment.ItemText>{props.children}</SelectCompoment.ItemText>
+    </SelectCompoment.Item>
+  );
+}
