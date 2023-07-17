dba1d4d68c31d0ccc54a3e19c65d75a6514f57ea
Fred Liang <info@fredliang.cn>
2023-07-17 23:30:53 +0800

    feat: add reddot to click item

--- packages/chrome/src/layouts/AppLayout/Menu/index.tsx
+++ packages/chrome/src/layouts/AppLayout/Menu/index.tsx
@@ -7,15 +7,18 @@ import { ReactComponent as IconNFT } from '../../../assets/icons/nft.svg';
 import { ReactComponent as IconHistory } from '../../../assets/icons/history.svg';
 import { ReactComponent as IconGrid } from '../../../assets/icons/grid.svg';
 import { ReactComponent as IconSwap } from '../../../assets/icons/swap.svg';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useFeatureFlagsWithNetwork } from '../../../hooks/useFeatureFlags';
+import { useLocalStorageState } from 'ahooks';
 
 export type MenuProps = StyleExtendable;
 type MenuItemProps = StyleExtendable & {
   active?: boolean;
   icon: ReactNode;
   alt?: string;
+  onClick?: () => void;
   to: string;
+  redDot?: boolean;
 };
 
 const MenuItem = (props: MenuItemProps) => {
@@ -25,13 +28,21 @@ const MenuItem = (props: MenuItemProps) => {
 
   return (
     <div
-      className={classnames(styles['menu-item'], {
+      className={classnames(styles['menu-item'], 'relative', {
         [styles['menu-item--active']]: match,
       })}
       onClick={() => {
         navigate(props.to);
+        props.onClick?.();
       }}
     >
+      {props.redDot && (
+        <div
+          className={
+            'rounded absolute bg-red-300 w-2 h-2 right-[5px] top-[5px]'
+          }
+        />
+      )}
       <div className={styles['menu-icon']}>{props.icon}</div>
     </div>
   );
@@ -39,7 +50,9 @@ const MenuItem = (props: MenuItemProps) => {
 
 const Menu: React.FC<MenuProps> = (props: Extendable) => {
   const featureFlags = useFeatureFlagsWithNetwork();
-
+  const [clickedSwap, setClickedSwap] = useLocalStorageState('clicked-swap', {
+    defaultValue: false,
+  });
   return (
     <div
       className={classnames(styles['menu'], props.className)}
@@ -48,7 +61,13 @@ const Menu: React.FC<MenuProps> = (props: Extendable) => {
       <MenuItem to="/home" icon={<IconHome />} alt="home" />
       <MenuItem to="/nft" icon={<IconNFT />} alt="nft" />
       {featureFlags?.enable_swap && (
-        <MenuItem to="/swap" icon={<IconSwap />} alt="swap" />
+        <MenuItem
+          redDot={!clickedSwap}
+          onClick={() => setClickedSwap(true)}
+          to="/swap"
+          icon={<IconSwap />}
+          alt="swap"
+        />
       )}
       <MenuItem to="/dapps" icon={<IconGrid />} alt="dapps" />
       <MenuItem
