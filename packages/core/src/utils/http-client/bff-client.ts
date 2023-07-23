f25085b5cac074e8bb3d5a6a92ad3942e936e540
brrruski <sizhe4real@gmail.com>
2023-07-23 00:34:07 -0400

    feat: add client for bff node service

---
+++ packages/core/src/utils/http-client/bff-client.ts
@@ -0,0 +1,3 @@
+import { HttpClient } from './http-client';
+
+export const bffClient = new HttpClient('http://localhost:3000');
---
+++ packages/core/src/utils/http-client/index.ts
@@ -0,0 +1,2 @@
+export * from './http-client';
+export * from './bff-client';
